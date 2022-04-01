5a28e358ffd5db613e289632d088a0d4673a3c4c
ruger.crackme <550279039@qq.com>
2022-04-01 10:42:41 +0800

    search friends

--- app/src/main/java/com/chatty/compose/screens/contracts/AddFriends.kt
+++ app/src/main/java/com/chatty/compose/screens/contracts/AddFriends.kt
@@ -1,13 +1,18 @@
 package com.chatty.compose.screens.contracts
 
+import android.util.Log
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.focusable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -32,26 +37,47 @@ import androidx.compose.ui.semantics.SemanticsProperties.ImeAction
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.toLowerCase
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.chatty.compose.R
-import com.chatty.compose.ui.components.AppScreen
-import com.chatty.compose.ui.components.CenterRow
-import com.chatty.compose.ui.components.HeightSpacer
-import com.chatty.compose.ui.components.WidthSpacer
+import com.chatty.compose.bean.UserProfileData
+import com.chatty.compose.screens.chatty.mock.friends
+import com.chatty.compose.ui.components.*
 import com.chatty.compose.ui.utils.LocalNavController
 import com.chatty.compose.ui.utils.hideIME
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.launch
 import kotlin.math.max
 import java.util.*
+import kotlin.random.Random
 
+
+
+// 后续可以提升到 ViewModel 中
+var isLoading by mutableStateOf<Boolean>(false)
+var displaySearchUsersFlow = MutableStateFlow<List<UserProfileData>>(listOf())
+
+suspend fun refreshFriendSearched(searchContent: String) {
+    delay(3000)
+    var currentResult = friends.filter {
+        it.nickname.lowercase(Locale.getDefault()).contains(searchContent)
+    }.toMutableList()
+    displaySearchUsersFlow.emit(currentResult)
+    isLoading = false
+}
 
 @Preview
 @Composable
 fun AddFriends() {
     var isSearchingState = remember { mutableStateOf(false) }
+    var displaySearchUsers = displaySearchUsersFlow.collectAsState()
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -62,6 +88,22 @@ fun AddFriends() {
         }
         HeightSpacer(value = 5.dp)
         SearchFriendBar(isSearchingState)
+        HeightSpacer(value = 10.dp)
+        if(!isSearchingState.value) {
+            HeightSpacer(value = 10.dp)
+            AddFriendsOtherWay()
+        } else {
+            if (isLoading) {
+                CircularProgressIndicator(modifier = Modifier.align(Alignment.CenterHorizontally))
+            }
+            LazyColumn {
+                displaySearchUsers.value.forEach {
+                    item {
+                        FriendItem(avatarRes = it.avatarRes, friendName = it.nickname, motto = it.motto)
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -94,7 +136,7 @@ fun AddFriendTopBar() {
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun SearchFriendBar(isSearchingState: MutableState<Boolean>) {
-    val context = LocalContext.current
+    val scope = rememberCoroutineScope()
     var focusManager = LocalFocusManager.current
     var searchContent by remember { mutableStateOf("") }
     var focusRequester = remember { FocusRequester() }
@@ -123,7 +165,19 @@ fun SearchFriendBar(isSearchingState: MutableState<Boolean>) {
                         isSearchingState.value = it.isFocused
                     },
                 textStyle = TextStyle(fontSize = 18.sp),
-                maxLines = 1
+                maxLines = 1,
+                keyboardActions = KeyboardActions(
+                    onSearch = {
+                        scope.launch {
+                            isLoading = true
+                            refreshFriendSearched(searchContent)
+                        }
+                    }
+                ),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text,
+                    imeAction = androidx.compose.ui.text.input.ImeAction.Search
+                )
             ) { innerText ->
                 CenterRow(Modifier.fillMaxWidth()) {
                     Box(
@@ -163,6 +217,8 @@ fun SearchFriendBar(isSearchingState: MutableState<Boolean>) {
         cancel = {
             if (isSearchingState.value) {
                 TextButton(onClick = {
+                    displaySearchUsersFlow.tryEmit(emptyList())
+                    isLoading = false
                     focusManager.clearFocus()
                     isSearchingState.value = false
                     searchContent = ""
@@ -198,6 +254,67 @@ fun SubcompositionRow(modifier: Modifier, textField: @Composable () -> Unit, can
         layout(width, height) {
             textFieldPlaceables.placeRelative(0, 0)
             cancelPlaceable?.placeRelative(textFieldPlaceables.width, 0)
+        }
+    }
+}
+
+@Composable
+fun AddFriendsOtherWay() {
+    Column() {
+        AddFriendsOtherWay(
+            R.drawable.qr_code,
+            "扫一扫",
+            "扫描二维码添加联系人"
+        )
+    }
+}
+
+@Composable
+fun AddFriendsOtherWay(
+    icon: Int,
+    functionName: String,
+    description: String
+) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable {
+
+            },
+        color = Color(0xFFF8F8F8)
+    ) {
+        CenterRow(
+            modifier = Modifier.padding(vertical = 8.dp, horizontal = 10.dp)
+        ) {
+            Icon(
+                painter =  painterResource(id = R.drawable.qr_code),
+                contentDescription = "qr_code",
+                modifier = Modifier
+                    .size(60.dp)
+                    .padding(12.dp)
+            )
+            Spacer(Modifier.padding(horizontal = 10.dp))
+            Column(
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(
+                    text = functionName,
+                    style = MaterialTheme.typography.h6
+                )
+                Spacer(Modifier.padding(vertical = 3.dp))
+                Text(
+                    text = description,
+                    style = MaterialTheme.typography.body2,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
+            WidthSpacer(4.dp)
+            Column(
+                horizontalAlignment = Alignment.End
+            ) {
+                Icon(painter = painterResource(id = R.drawable.expand_right), contentDescription = "")
+            }
         }
     }
 }
