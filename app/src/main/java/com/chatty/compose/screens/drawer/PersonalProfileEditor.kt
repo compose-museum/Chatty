a97776447f01dd0c8718e8178bfd6266a94ce792
ruger.crackme <550279039@qq.com>
2022-04-03 10:27:01 +0800

    opt: 用户信息编辑页样式调整

--- app/src/main/java/com/chatty/compose/screens/drawer/PersonalProfileEditor.kt
+++ app/src/main/java/com/chatty/compose/screens/drawer/PersonalProfileEditor.kt
@@ -3,6 +3,8 @@ package com.chatty.compose.screens.drawer
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -17,6 +19,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.chatty.compose.R
 import com.chatty.compose.ui.components.AppScreen
+import com.chatty.compose.ui.components.TopBar
 import com.chatty.compose.ui.components.WidthSpacer
 import com.chatty.compose.ui.theme.ok
 import com.chatty.compose.ui.utils.LocalNavController
@@ -31,24 +34,16 @@ fun Demo() {
 @Composable
 fun PersonalProfileEditor(title: String, isGender: Boolean = false, isQRCode: Boolean) {
     val navController = LocalNavController.current
-    var selectMale by remember {
-        mutableStateOf(false)
-    }
-    Scaffold(
-        topBar = {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(80.dp)
-                    .padding(horizontal = 15.dp),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Button(onClick = {
-                    navController.navigate(AppScreen.main)
+    Column {
+        TopBar(
+            start = {
+                IconButton(onClick = {
+                    navController.popBackStack()
                 }) {
-                    Text(text = "取消")
+                    Icon(Icons.Rounded.ArrowBack, null)
                 }
+            },
+            center =  {
                 Text(
                     text = title,
                     fontSize = 20.sp,
@@ -56,108 +51,124 @@ fun PersonalProfileEditor(title: String, isGender: Boolean = false, isQRCode: Bo
                     color = Color.Black,
                     modifier = Modifier.padding(start = 20.dp)
                 )
-                Button(onClick = {
-                    navController.navigate(AppScreen.main)
-                }) {
-                    Text(text = "完成")
+            },
+            end =  {
+                Button(
+                    onClick = { navController.navigate(AppScreen.main) },
+                    colors = ButtonDefaults.buttonColors(
+                        backgroundColor = ok
+                    ),
+                ) {
+                    Text(text = "完成", color = Color.White)
                 }
-            }
-        },
-        modifier =  Modifier.systemBarsPadding()
-    ) {
+            },
+            backgroundColor = Color.White
+        )
         if (isGender) {
-            Column(modifier = Modifier.fillMaxWidth()) {
-                Row(
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .clickable {
-                            selectMale = true
-                        }
-                        .padding(horizontal = 20.dp)
-                        .height(50.dp)
-                ) {
-                    Row {
-                        Icon(
-                            painter = painterResource(id = R.drawable.male),
-                            contentDescription = "male",
-                            tint = Color.Blue
-                        )
-                        WidthSpacer(value = 3.dp)
-                        Text(
-                            text = "男",
-                            fontSize = 15.sp,
-                            fontWeight = FontWeight.ExtraBold,
-                            color = Color.Black
-                        )
-                    }
-                    if (selectMale) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.correct),
-                            contentDescription = "correct",
-                            tint = ok
-                        )
-                    }
-                }
-                Divider()
-                Row(
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .clickable {
-                            selectMale = false
-                        }
-                        .padding(horizontal = 20.dp)
-                        .height(50.dp)
-                ) {
-                    Row {
-                        Icon(
-                            painter = painterResource(id = R.drawable.female),
-                            contentDescription = "male",
-                            tint = Color(0xffd93a7d)
-                        )
-                        WidthSpacer(value = 3.dp)
-                        Text(
-                            text = "女",
-                            fontSize = 15.sp,
-                            fontWeight = FontWeight.ExtraBold,
-                            color = Color.Black
-                        )
-                    }
-                    if (!selectMale) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.correct),
-                            contentDescription = "correct",
-                            tint = ok
-                        )
-                    }
-                }
-            }
+            GenderSelector()
         } else {
-            var inputText by remember {
-                mutableStateOf("")
+            ProfileInputField()
+        }
+    }
+}
+
+@Composable
+fun ProfileInputField() {
+    var inputText by remember {
+        mutableStateOf("")
+    }
+    var focusRequester = remember {
+        FocusRequester()
+    }
+    OutlinedTextField(
+        value = inputText,
+        onValueChange = {
+            inputText = it
+        },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 20.dp)
+            .focusRequester(focusRequester),
+        maxLines = 1,
+        textStyle = TextStyle(
+            fontSize = 20.sp
+        ),
+    )
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
+    }
+}
+@Composable
+fun GenderSelector() {
+    var selectMale by remember {
+        mutableStateOf(false)
+    }
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable {
+                    selectMale = true
+                }
+                .padding(horizontal = 20.dp)
+                .height(80.dp)
+        ) {
+            Row {
+                Icon(
+                    painter = painterResource(id = R.drawable.male),
+                    contentDescription = "male",
+                    tint = Color.Blue
+                )
+                WidthSpacer(value = 3.dp)
+                Text(
+                    text = "男",
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.ExtraBold,
+                    color = Color.Black
+                )
             }
-            var focusRequester = remember {
-                FocusRequester()
+            if (selectMale) {
+                Icon(
+                    painter = painterResource(id = R.drawable.correct),
+                    contentDescription = "correct",
+                    tint = ok
+                )
             }
-            OutlinedTextField(
-                value = inputText,
-                onValueChange = {
-                    inputText = it
-                },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 20.dp)
-                    .focusRequester(focusRequester),
-                maxLines = 1,
-                textStyle = TextStyle(
-                    fontSize = 20.sp
-                ),
-            )
-            LaunchedEffect(Unit) {
-                focusRequester.requestFocus()
+        }
+        Divider()
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable {
+                    selectMale = false
+                }
+                .padding(horizontal = 20.dp)
+                .height(80.dp)
+        ) {
+            Row {
+                Icon(
+                    painter = painterResource(id = R.drawable.female),
+                    contentDescription = "male",
+                    tint = Color(0xffd93a7d)
+                )
+                WidthSpacer(value = 3.dp)
+                Text(
+                    text = "女",
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.ExtraBold,
+                    color = Color.Black
+                )
+            }
+            if (!selectMale) {
+                Icon(
+                    painter = painterResource(id = R.drawable.correct),
+                    contentDescription = "correct",
+                    tint = ok
+                )
             }
         }
     }
